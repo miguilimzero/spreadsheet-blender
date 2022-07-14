095dea33815be0c8846f4b4d1a706122522eb2a3
srdante <miguelarany@gmail.com>
2022-07-14 20:08:45 -0300

    fix dark theme

--- src/mixins/dark-theme.js
+++ src/mixins/dark-theme.js
@@ -1,40 +1,40 @@
 export default {
-    data: () => ({
-        currentTheme: '',
-    }),
+	data: () => ({
+		currentTheme: '',
+	}),
 
-    created() {
-        this.applyTheme()
-    },
+	created() {
+		this.applyTheme()
+	},
 
-    methods: {
-        changeTheme(newValue) {
-            localStorage.theme = newValue
+	methods: {
+		changeTheme(newValue) {
+			localStorage.theme = newValue
 
-            this.applyTheme()
-        },
+			this.applyTheme()
+		},
 
-        applyTheme() {
-            this.currentTheme = localStorage.theme
+		applyTheme() {
+			this.currentTheme = localStorage.theme
 
-            if (this.currentTheme === 'dark') {
-                document.querySelector('html').classList.add('dark')
+			if (this.currentTheme === 'dark') {
+				document.querySelector('html').classList.add('dark')
 
-                return
-            } 
-            
-            if (this.currentTheme === 'light') {
-                document.querySelector('html').classList.remove('dark')
+				return
+			}
 
-                return
-            }
+			if (this.currentTheme === 'light') {
+				document.querySelector('html').classList.remove('dark')
 
-            // Use system defined one
-            if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                document.querySelector('html').classList.add('dark')
-            } else {
-                document.querySelector('html').classList.remove('dark')
-            }
-        },
-    }
+				return
+			}
+
+			// Use system defined one
+			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				document.querySelector('html').classList.add('dark')
+			} else {
+				document.querySelector('html').classList.remove('dark')
+			}
+		},
+	},
 }
