686653d2621749be530c50c94f5249798e916aca
srdante <miguelarany@gmail.com>
2022-07-14 19:58:56 -0300

    wip dark theme

--- src/mixins/dark-theme.js
+++ src/mixins/dark-theme.js
@@ -1,13 +1,40 @@
 export default {
     data: () => ({
-
+        currentTheme: '',
     }),
 
-    computed: {
-
+    created() {
+        this.applyTheme()
     },
 
     methods: {
+        changeTheme(newValue) {
+            localStorage.theme = newValue
 
-    },
+            this.applyTheme()
+        },
+
+        applyTheme() {
+            this.currentTheme = localStorage.theme
+
+            if (this.currentTheme === 'dark') {
+                document.querySelector('html').classList.add('dark')
+
+                return
+            } 
+            
+            if (this.currentTheme === 'light') {
+                document.querySelector('html').classList.remove('dark')
+
+                return
+            }
+
+            // Use system defined one
+            if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.querySelector('html').classList.add('dark')
+            } else {
+                document.querySelector('html').classList.remove('dark')
+            }
+        },
+    }
 }
