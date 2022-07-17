10e37ef0c2a20c71358b6c9773518e8766d82a12
srdante <miguelarany@gmail.com>
2022-07-17 01:53:45 -0300

    move logic to mixin

--- src/mixins/language.js
+++ src/mixins/language.js
@@ -1,8 +1,9 @@
-import { SUPPORT_LOCALES, loadLocaleMessages, setI18nLanguage } from '@/i18n'
+import { nextTick } from 'vue'
 
 export default {
 	data: () => ({
 		currentLanguage: '',
+        supportedLanguages: ['en', 'pt', 'es'],
 	}),
 
 	created() {
@@ -11,7 +12,7 @@ export default {
 		if (!this.currentLanguage) {
 			const browserLanguage = (navigator.language || navigator.userLanguage).split('-')[0]
 
-			if (this.availableLanguages.includes(SUPPORT_LOCALES)) {
+			if (this.availableLanguages.includes(this.supportedLanguages)) {
 				this.currentLanguage = browserLanguage
 			} else {
 				this.currentLanguage = 'en'
@@ -35,9 +36,27 @@ export default {
 
 	methods: {
 		async initializeI18n() {
-			await loadLocaleMessages(this.$i18n, this.currentLanguage)
+			await this.loadLocaleMessages(this.$i18n, this.currentLanguage)
 
-			setI18nLanguage(this.$i18n, this.currentLanguage)
+			this.setI18nLanguage(this.$i18n, this.currentLanguage)
 		},
+        
+        async loadLocaleMessages(i18n, locale) {
+            if(locale === 'en') {
+                return nextTick()
+            }
+            
+            let messages = await import(/* webpackChunkName: "locale-[request]" */ `./../locales/${locale}.json`)
+        
+            i18n.setLocaleMessage(locale, messages.default)
+        
+            return nextTick()
+        },
+
+        setI18nLanguage(i18n, locale) {
+            i18n.locale = locale
+        
+            document.querySelector('html').setAttribute('lang', locale)
+        },
 	},
 }
